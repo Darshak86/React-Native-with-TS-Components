8283bebe783e9d8a60a76676ef360e148d48ac4d
Darshak Trivedi <79185967+Darshak86@users.noreply.github.com>
2022-08-05 10:00:58 +0530

    Start Demo
    basic start react native with type script demo

---
+++ RNC_TS_CLI_Demo/App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Welcome to react native with type script..!!</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
